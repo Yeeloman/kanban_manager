a78f7e2cc5dd327614ae999a0e1c1776ce0841fa
Yeeloman <yami.onlyme@gmail.com>
2024-05-19 17:46:14 +0100

    add hashin Password using sha1

--- src/lib/db/quiries.ts
+++ src/lib/db/quiries.ts
@@ -1,15 +1,23 @@
 import db from '@/db/db';
-import { hash } from 'bcrypt';
+import sha1 from 'sha1';
 // import type { signUpType } from '@/FormSchema/FormSchema';
 import { usersTable } from './schema';
 
 type newUser = typeof usersTable.$inferInsert;
 
 export const signUp = async (form: newUser) => {
-    console.log('adding the data to the db')
+    try {
 
-
-    await db.insert(usersTable).values(form);
+        const { password } = form;
+        const hashedPwd = await sha1(password as string)
+        await db.insert(usersTable).values({
+            username: form.username,
+            email: form.email,
+            password: hashedPwd
+        });
+    } catch (e) {
+        console.log("Error regestring to db: ", e);
+    }
 
 
 }

45ea84ce225fd3a93aa7c7348ca0ad82a6e5780a
Yeeloman <yami.onlyme@gmail.com>
2024-07-05 12:44:18 +0100

    add .env

--- src/lib/db/db.server.ts
+++ src/lib/db/db.server.ts
@@ -1,14 +1,15 @@
 import * as schema from './schema';
-//import { YEELODB_URL } from '$env/static/private';
+import { YEELODB_URL } from '$env/static/private';
 import postgres from 'postgres';
 import { drizzle } from 'drizzle-orm/postgres-js';
-import 'dotenv/config'
+// import 'dotenv/config'
 
 
-const client = postgres(process.env.YEELODB_URL);
+const client = postgres(YEELODB_URL);
+// const client = postgres(process.env.YEELODB_URL as string);
 
 const db = drizzle(client, {
-    schema:schema
+  schema: schema
 })
 
 export default db;
--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -12,89 +12,90 @@ import { Scrypt } from "lucia";
 const scrypt = new Scrypt();
 
 export const load: PageServerLoad = (async () => {
-    const signInForm = await superValidate(zod(signInSchema));
-    const signUpForm = await superValidate(zod(signUpSchema));
+  const signInForm = await superValidate(zod(signInSchema));
+  const signUpForm = await superValidate(zod(signUpSchema));
 
-    return {
-        form: {
-            signInForm,
-            signUpForm
-        }
-    };
+  return {
+    form: {
+      signInForm,
+      signUpForm
+    }
+  };
 });
 
 export const actions: Actions = {
-    signIn: async({request, cookies }: { request: Request, cookies: Cookies }) => {
-        const signInForm = await superValidate(request, zod(signInSchema));
+  signIn: async ({ request, cookies }: { request: Request, cookies: Cookies }) => {
+    const signInForm = await superValidate(request, zod(signInSchema));
 
-        if (!signInForm.valid) {
-            return fail(400, {
-                signInForm
-            });
-        }
-
-        const username = signInForm.data.username;
-        const password = signInForm.data.password;
-
-        try {
-            const userExist = await checkIfNameExist(username);
-            if (userExist === false) {
-                return setError(signInForm, 'username', 'Username does not exist');
-            }
-
-
-            const user = await getUserByName(username);
-
-            const hash = user[0].password;
-
-            const validPassword = await scrypt.verify(hash, password);
-            if (validPassword) {
-                return setError(signInForm, 'password', 'Incorrect password');
-            }
-
-            await getSessionByUserId(lucia, user[0].id, cookies);
-
-        } catch (e) {
-            console.log('Error:', e);
-        }
-
-        throw redirect(303, `/p/${username}`)
-
-    },
-
-    signUp: async ({ request, cookies }: { request: Request, cookies: Cookies }) => {
-        const signUpForm = await superValidate(request, zod(signUpSchema));
-        console.log("🚀 ~ signUp: ~ signUpForm:", signUpForm)
-
-        if (!signUpForm.valid) {
-            return fail(400, {
-                signUpForm
-            });
-        }
-
-        try {
-            const emailExist = await checkIfEmailExist(signUpForm.data.email);
-            if (emailExist === true) {
-                return setError(signUpForm, 'email', 'Email already registered');
-            }
-
-            const userId = generateId(15);
-            const hash = await scrypt.hash(signUpForm.data.password);
-
-            await insertNewUser({
-                id: userId,
-                username: signUpForm.data.username,
-                email: signUpForm.data.email,
-                password: hash
-            });
-
-            await createAndSetSession(lucia, userId, cookies);
-
-        } catch (e) {
-            console.log('Error:', e);
-        }
-
-        throw redirect(303, `/p/${signUpForm.data.username}`)
-        // return message(signUpForm, "OK");
+    if (!signInForm.valid) {
+      return fail(400, {
+        signInForm
+      });
     }
+
+    const username = signInForm.data.username;
+    const password = signInForm.data.password;
+
+    try {
+      const userExist = await checkIfNameExist(username);
+      if (userExist === false) {
+        return setError(signInForm, 'username', 'Username does not exist');
+      }
+
+
+      const user = await getUserByName(username);
+
+      const hash = user[0].password;
+
+      const validPassword = await scrypt.verify(hash, password);
+      if (validPassword) {
+        return setError(signInForm, 'password', 'Incorrect password');
+      }
+
+      await getSessionByUserId(lucia, user[0].id, cookies);
+
+    } catch (e) {
+      console.log('Error:', e);
+    }
+
+    throw redirect(303, `/p/${username}`)
+
+  },
+
+  signUp: async ({ request, cookies }: { request: Request, cookies: Cookies }) => {
+    const signUpForm = await superValidate(request, zod(signUpSchema));
+    console.log("🚀 ~ signUp: ~ signUpForm:", signUpForm)
+
+    if (!signUpForm.valid) {
+      return fail(400, {
+        signUpForm
+      });
+    }
+
+    try {
+      console.log('here?')
+      const emailExist = await checkIfEmailExist(signUpForm.data.email);
+      if (emailExist === true) {
+        return setError(signUpForm, 'email', 'Email already registered');
+      }
+
+      const userId = generateId(15);
+      const hash = await scrypt.hash(signUpForm.data.password);
+
+      await insertNewUser({
+        id: userId,
+        username: signUpForm.data.username,
+        email: signUpForm.data.email,
+        password: hash
+      });
+
+      await createAndSetSession(lucia, userId, cookies);
+
+    } catch (e) {
+      console.log('Error sign up:', e);
+    }
+
+    throw redirect(303, `/p/${signUpForm.data.username}`)
+    // return message(signUpForm, "OK");
+  }
 }
